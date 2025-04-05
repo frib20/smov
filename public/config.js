471dc6d43621adfebd4c29fa9affd0d63c33d0b3
frib20 <80797028+frib20@users.noreply.github.com>
2025-04-05 01:11:37 -0400

    Update config.js for another service

--- public/config.js
+++ public/config.js
@@ -3,7 +3,8 @@ window.__CONFIG__ = {
   // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
   VITE_CORS_PROXY_URL: "https://proxy.sudo-flix.nl",
 
-  VITE_BACKEND_URL: "https://server.fifthwit.net",
+ // VITE_BACKEND_URL: "https://server.fifthwit.net",
+  VITE_BACKEND_URL: "https://backend.movies.samj.app",
 
   // The READ API key to access TMDB
   VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzODcyMjg2MDgxZDU5ZTcxYjQ1ZTBhYzUwMGYwNmE4MCIsIm5iZiI6MTc0MzczMTM5Mi4xMjEsInN1YiI6IjY3ZWYzYWMwOGIxZjMyZWI3OWQ5MTBiNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jvpgPsAF4cdTVIncERqPk1vkPs1ttP5nbilAtkPdoPo",
@@ -12,7 +13,7 @@ window.__CONFIG__ = {
   VITE_DMCA_EMAIL: null,
 
   // Whether to disable hash-based routing, leave this as false if you don't know what this is
-  VITE_NORMAL_ROUTER: true,
+  Quickly_normal_router: true,
 
   // The backend URL to communicate with
 
